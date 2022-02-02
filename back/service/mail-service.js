be563c4bcdeb486f1e1adbf61b6e35b1bb488b7c
Mariam Arakelyan <mariama@wizardsdev.com>
2022-02-02 21:39:54 +0400

    regirstation back part

--- back/service/mail-service.js
+++ back/service/mail-service.js
@@ -1,6 +1,33 @@
-class MailService {
-    async sendActivationMail(to, link) {
+const nodeMailer = require('nodemailer');
 
+class MailService {
+
+    constructor() {
+        this.transporter = nodeMailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port:process.env.SMTP_PORT,
+            secure: false,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD,
+            }
+        })
+    }
+
+
+    async sendActivationMail(to, link) {
+        await this.transporter.sendMail({
+            from: process.env.SMTP_USER,
+            to,
+            subject: 'Activation of account ' + process.env.API_URL,
+            text: '',
+            html:
+                `<div>
+                    <h1>Link For activation</h1>
+                    <a href="${link}">${link}</a>
+                </div>
+                `
+        })
     }
 }
 
--- back/service/user-service.js
+++ back/service/user-service.js
@@ -2,10 +2,13 @@ const UserModel = require('../models/user-model');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const mailService = require('../service/mail-service');
+const tokenService = require('../service/token-service');
+const UserDto = require('../dtos/user-dto');
 
 class UserService {
     async registration(email, password) {
         const candidate = await UserModel.findOne(email);
+
         if(candidate) {
             throw new Error(`User with ${email} email already exist`);
         }
@@ -13,8 +16,16 @@ class UserService {
         const hashPassword = await bcrypt.hash(password, 3);
         const activationLink = uuid.v4();
         const user = await UserModel.create({email, password: hashPassword, activationLink});
-        await mailService.sendActivationMail(email, activationLink);
+        await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
 
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateToken({...userDto});
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return {
+            ...tokens,
+            user: userDto
+        };
     }
 }
 
