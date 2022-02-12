3684ca7083bff7fe19f2049a395e024abfcef686
Mariam Arakelyan <mariama@wizardsdev.com>
2022-02-13 02:21:20 +0400

    some changes

---
+++ front/my-app/src/components/LoginForm.tsx
@@ -0,0 +1,28 @@
+import React, {FC, useState} from 'react';
+
+
+const LoginForm: FC = () => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+
+    return(
+        <div>
+            <input
+                type='text'
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                placeholder='Email'
+            />
+            <input
+                type='password'
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+                placeholder='Password'
+            />
+            <button>Login</button>
+            <button>Password</button>
+        </div>
+    );
+};
+
+export default LoginForm;
